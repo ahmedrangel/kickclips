d8f219399f5e4ff05a35973e6c24c5e9fa275632
Ahmed Rangel <50090595+ahmedrangel@users.noreply.github.com>
2023-06-26 17:55:13 -0500

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -150,8 +150,8 @@
           date: this.getDate(data.clip.created_at),
           duration: (() => {
             const durationSeconds = data.clip.duration;
-            let minutes = (Math.floor(durationSeconds / 60)).toString().padStart(2, "0");;
-            let seconds = (durationSeconds % 60).toString().padStart(2, "0");;
+            const minutes = (Math.floor(durationSeconds / 60)).toString().padStart(2, "0");;
+            const seconds = (durationSeconds % 60).toString().padStart(2, "0");;
             const formattedTime = minutes + ":" + seconds;
             return formattedTime;
           })()
