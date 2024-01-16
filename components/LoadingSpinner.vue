9762705860aed93ca980930c40b06c9ef12962ab
Ahmed Rangel <ahmedrangel@outlook.com>
2024-01-16 05:45:15 -0500

    processing text to loading spinner

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -3,5 +3,6 @@
     <div class="spinner-border spinner-lg" role="status">
       <span class="visually-hidden">Loading...</span>
     </div>
+    <span class="ms-2">Processing...</span>
   </div>
 </template>
