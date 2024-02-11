da36692a2dbf8b18958182d0493df7e6ec83aae6
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-11 05:48:49 -0500

    Revert "chore: add nuxtjs sitemap"
    This reverts commit 673694c6e99c50559cd292e7d1fa56b1cd42e3e7.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,24 +31,6 @@ export default defineNuxtConfig({
     inlineStyles: false
   },
   modules: [
-    "nuxt-icon",
-    "@nuxtjs/sitemap"
-  ],
-  nitro: {
-    prerender: {
-      routes: ["/sitemap.xml"],
-    }
-  },
-  sitemap: {
-    dynamicUrlsApiEndpoint: "/__sitemap",
-    xslColumns: [
-      { label: "URL", width: "65%" },
-      { label: "Priority", select: "sitemap:priority", width: "12.5%" },
-      { label: "Last Modified", select: "sitemap:lastmod", width: "35%" }
-    ]
-  },
-  routeRules: {
-    "/": { sitemap: { priority: 1 } },
-    "/*/**": { sitemap: { priority: 0.8, lastmod: new Date().toISOString() } }
-  }
+    "nuxt-icon"
+  ]
 });
