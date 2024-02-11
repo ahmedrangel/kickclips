f1bdceb2c4466d4fd8c49846224823d6b19821da
Ahmed Rangel <ahmedrangel@outlook.com>
2024-02-11 06:17:38 -0500

    chore: updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,6 +45,7 @@ export default defineNuxtConfig({
     }
   },
   sitemap: {
+    dynamicUrlsApiEndpoint: "/__sitemap",
     xslColumns: [
       { label: "URL", width: "65%" },
       { label: "Priority", select: "sitemap:priority", width: "12.5%" },
