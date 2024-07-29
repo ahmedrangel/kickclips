4f24c23b16c4a195987b90b630fa52d50e56da08
Ahmed Rangel <ahmedrangel@outlook.com>
2024-07-29 05:30:15 -0500

    chore: update trigger endpoint

--- server/api/clip/[id].ts
+++ server/api/clip/[id].ts
@@ -2,7 +2,7 @@ import { RESOURCES } from "~/utils/INFO";
 
 export default defineEventHandler(async (event): Promise<{ url: string } | null> => {
   const { id } = getRouterParams(event);
-  const triggerTmp = await $fetch(`${RESOURCES.trigger}/api/kick/clip/${id}`).catch(() => null) as { url: string };
+  const triggerTmp = await $fetch(`${RESOURCES.trigger}/api/kick/clip/${id}/download`).catch(() => null) as { url: string };
   if (triggerTmp?.url) return { url: triggerTmp.url };
 
   const crossClip = await $fetch(`${RESOURCES.worker}/kick/clip/${id}`).catch(() => null) as { url: string };
