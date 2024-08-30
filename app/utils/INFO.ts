9b2c01cfe28ff7da4dbf350f2be1514fbcbb6ad8
Ahmed Rangel <ahmedrangel@outlook.com>
2024-08-30 12:37:40 -0500

    chore: update cdn prefix and cache cdn put cache control

--- app/utils/INFO.ts
+++ app/utils/INFO.ts
@@ -3,7 +3,7 @@ export const RESOURCES = {
   kickClipsTmp: "https://clips.kick.com/tmp",
   worker: "https://dev.ahmedrangel.com",
   trigger: "https://kick-trigger.ahmedrangel.com",
-  cdn: "https://cdn.ahmedrangel.com/videos/kick"
+  cdn: "https://cdn.ahmedrangel.com/tmp/videos/kick"
 };
 
 export const SITE = {
--- server/api/cdn.put.ts
+++ server/api/cdn.put.ts
@@ -4,11 +4,11 @@ export default defineEventHandler(async (event) => {
   const { cloudflare } = event.context;
   const config = useRuntimeConfig(event)
   const fd = await readFormData(event)
-  fd.append("prefix", "videos/kick");
+  fd.append("prefix", "tmp/videos/kick");
   fd.append("httpMetadata", JSON.stringify({
     "Content-Type": "video/mp4",
     "Content-Disposition": "inline",
-    "Cache-Control": "public, max-age=432000"
+    "Cache-Control": "public, max-age=86400"
   }))
 
   const cdn = async () => {
