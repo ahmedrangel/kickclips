4f4d6f1a6b4b6404ee0ccaf1b40d2d57626e06ce
Ahmed Rangel <ahmedrangel@outlook.com>
2024-08-30 12:38:19 -0500

    chore: lint

--- server/api/cdn.put.ts
+++ server/api/cdn.put.ts
@@ -2,14 +2,14 @@ import { RESOURCES } from "~/utils/INFO";
 
 export default defineEventHandler(async (event) => {
   const { cloudflare } = event.context;
-  const config = useRuntimeConfig(event)
-  const fd = await readFormData(event)
+  const config = useRuntimeConfig(event);
+  const fd = await readFormData(event);
   fd.append("prefix", "tmp/videos/kick");
   fd.append("httpMetadata", JSON.stringify({
     "Content-Type": "video/mp4",
     "Content-Disposition": "inline",
     "Cache-Control": "public, max-age=86400"
-  }))
+  }));
 
   const cdn = async () => {
     return await $fetch(`${RESOURCES.worker}/cdn`, {
@@ -17,7 +17,7 @@ export default defineEventHandler(async (event) => {
       headers: { "x-cdn-auth": config.cdnToken },
       body: fd
     }).catch(() => null);
-  }
+  };
 
   if (import.meta.dev) {
     return cdn();
