e2840435733b9bd45ec5013312b7eb3af4afa8c0
Ahmed Rangel <ahmedrangel@outlook.com>
2025-07-13 02:41:53 -0500

    feat: add back to top

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <NuxtLoadingIndicator :throttle="0" />
+    <BackToTop />
     <NuxtPage />
   </div>
 </template>
---
+++ app/components/BackToTop.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const scrolled = ref(false);
+const maxScroll = 200;
+
+const topFunction = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+};
+
+onMounted(() => {
+  window.onscroll = () => {
+    scrolled.value = (document.body.scrollTop > maxScroll || document.documentElement.scrollTop > maxScroll);
+  };
+});
+</script>
+
+<template>
+  <Transition name="tab">
+    <button v-if="scrolled" id="back-to-top" class="bg-primary top rounded position-fixed m-4 end-0 bottom-0 btn text-dark btn-lg shadow d-none d-lg-block align-items-center" role="button" @click="topFunction()">
+      <span class="p-2 d-flex align-items-center">
+        <Icon name="ph:arrow-up-fill" size="1.5rem" />
+      </span>
+    </button>
+  </Transition>
+</template>
+
+<style scoped>
+.top {
+  z-index: 10021;
+}
+</style>
