1be9feccb2f66b8d03380e1b897de1bc567dd111
Ahmed Rangel <ahmedrangel@outlook.com>
2025-07-13 20:47:41 -0500

    chore: try authorization headers

--- server/api/channel/[slug]/clips.ts
+++ server/api/channel/[slug]/clips.ts
@@ -1,8 +1,14 @@
 export default defineEventHandler(async (event) => {
   const { slug } = getRouterParams(event);
   const { sort, time } = getQuery(event);
+  const config = useRuntimeConfig(event);
+  const kickToken = config.kickToken;
   const data = await $fetch<{ clips: GetClipResponse["clip"][], nextCursor: string }>(`${RESOURCES.apiV2}/channels/${slug}/clips`, {
     query: { sort, time },
+    headers: {
+      "User-Agent": "Cloudflare Workers/KickClips",
+      "Authorization": `Bearer ${kickToken}`
+    },
     parseResponse: JSON.parse
   }).catch(() => null);
   return data;
